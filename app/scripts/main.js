f3737b56b10128d69deded000c57f57762476d8f
crsimal <ottisbbx@gmail.com>
2015-04-30 12:53:07 +0200

    Imprimir tabla

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,32 @@
-console.log('\'Allo \'Allo!');
+var $boton = "#enviar";
+
+$($boton).click(function() {
+
+    var promesa = $.ajax({
+        url: 'http://www.media.formandome.es/phonegap/tutorial/futbolistas.php',
+        dataType: 'json'
+    })
+
+    promesa.done(function(futbolistas) {
+        console.log("success");
+        $($boton).css('display', 'none');
+
+        var fila, filas;
+        $.each(futbolistas, function(index, futbolista) {
+        	var fila;
+           $.each(futbolista, function(index, val) {
+           	 	fila += '<td>' + val + '</td>';
+           });
+           filas += '<tr>' + fila + '</tr>';
+        });
+
+        $('#listado tbody').append(filas);
+        $('#listado').css('display', 'block');
+
+    });
+
+    promesa.fail(function() {
+        console.log("error");
+    });
+
+});
